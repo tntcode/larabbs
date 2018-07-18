fe0b39c88a283629ba7827b05f79d1383637bf27
Yrj_Home <52145489@qq.com>
2018-07-18 21:49:21 +0800

    notifications stats

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -7,10 +7,25 @@ use App\Transformers\NotificationTransformer;
 
 class NotificationsController extends Controller
 {
+    /**
+     * 消息通知列表
+     * @return [type] [description]
+     */
     public function index()
     {
         $notifications = $this->user->notifications()->paginate(20);
 
         return $this->response->paginator($notifications, new NotificationTransformer());
+    }
+
+    /**
+     * 未读消息统计
+     * @return [type] [description]
+     */
+    public function stats()
+    {
+        return $this->response->array([
+            'unread_count' => $this->user()->notification_count,
+        ]);
     }
 }
