254d6359ffd41fbc825d1b4489f9c1ac48e9921c
Yrj_Home <52145489@qq.com>
2018-07-18 10:49:24 +0800

    api/user

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\UserRequest;
 
 class UsersController extends Controller
 {
+    /**
+     * 手机验证码注册
+     * @param  UserRequest $request [description]
+     * @return [type]               [description]
+     */
     public function store(UserRequest $request)
     {
         $verifyData = \Cache::get($request->verification_key);
@@ -30,6 +36,17 @@ class UsersController extends Controller
         // 清除验证码缓存
         \Cache::forget($request->verification_key);
 
-        return $this->response->created();
+        return $this->response->item($user, new UserTransformer())
+        ->setMeta([
+            'access_token' => \Auth::guard('api')->fromUser($user),
+            'token_type' => 'Bearer',
+            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+        ])
+        ->setStatusCode(201);
+    }
+
+    public function me()
+    {
+        return $this->response->item($this->user(), new UserTransformer());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,10 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api) {
 
+    //注册、登陆相关
     $api->group([
         'middleware' => 'api.throttle',
         'limit' => config('api.rate_limits.sign.limit'),
@@ -45,5 +47,21 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+    });
+
+    //访问相关
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
+        // 游客可以访问的接口
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
     });
 });
