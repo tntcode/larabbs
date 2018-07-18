4c9ab6c2d8c41e29873d40d71c4c9c799d5e5fe1
Yrj_Home <52145489@qq.com>
2018-07-18 16:44:19 +0800

    topic index

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -3,12 +3,58 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Transformers\TopicTransformer;
 use App\Http\Requests\Api\TopicRequest;
 
 class TopicsController extends Controller
 {
+
+    /**
+     * 话题列表
+     * @param  Request $request [description]
+     * @param  Topic   $topic   [description]
+     * @return [type]           [description]
+     */
+    public function index(Request $request, Topic $topic)
+    {
+        $query = $topic->query();
+
+        if ($categoryId = $request->category_id) {
+            $query->where('category_id', $categoryId);
+        }
+
+        // 为了说明 N+1问题，不使用 scopeWithOrder
+        switch ($request->order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        $topics = $query->paginate(20);
+
+        return $this->response->paginator($topics, new TopicTransformer());
+    }
+
+    /**
+     * 用户的话题列表
+     * @param  User    $user    [description]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function userIndex(User $user, Request $request)
+    {
+        $topics = $user->topics()->recent()
+            ->paginate(20);
+
+        return $this->response->paginator($topics, new TopicTransformer());
+    }
+
     /**
      * 创建话题
      * @param  TopicRequest $request [description]
