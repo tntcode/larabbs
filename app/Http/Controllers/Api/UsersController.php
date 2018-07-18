31382a8fdc65e1e8d284b31988900bdea503eb5b
Yrj_Home <52145489@qq.com>
2018-07-18 14:15:12 +0800

    user update

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\UserRequest;
+use App\Models\Image;
 
 class UsersController extends Controller
 {
@@ -45,8 +46,28 @@ class UsersController extends Controller
         ->setStatusCode(201);
     }
 
+    /**
+     * 获取个人信息
+     * @return [type] [description]
+     */
     public function me()
     {
         return $this->response->item($this->user(), new UserTransformer());
+    }
+
+    public function update(UserRequest $request)
+    {
+        $user = $this->user();
+
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+
+            $attributes['avatar'] = $image->path;
+        }
+        $user->update($attributes);
+
+        return $this->response->item($user, new UserTransformer());
     }
 }
