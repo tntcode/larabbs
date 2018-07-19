0878eeafaa35c51c488a4f1fd99778e727dada38
Yrj_Home <52145489@qq.com>
2018-07-19 16:34:07 +0800

    links index

--- routes/api.php
+++ routes/api.php
@@ -82,6 +82,10 @@ $api->version('v1', [
         $api->get('topics/{topic}', 'TopicsController@show')
             ->name('api.topics.show');
 
+        // 资源推荐
+        $api->get('links', 'LinksController@index')
+            ->name('api.links.index');
+
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
             // 当前登录用户信息
